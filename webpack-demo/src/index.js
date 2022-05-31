9b35f928c9ffc456897cf4df2771c6a1d2d0e121
Gabriel <gopxfs@hotmail.com>
2022-05-31 19:26:46 -0300

    Populate html elements

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,14 +1,53 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const tasksDiv = document.getElementById('to-do-list');
 
-// Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const listItems = [
+  {
+    description: 'description1',
+    isCompleted: false,
+    index: 0,
+  },
+  {
+    description: 'description2',
+    isCompleted: false,
+    index: 3,
+  },
+  {
+    description: 'description3',
+    isCompleted: true,
+    index: 2,
+  },
+  {
+    description: 'description4',
+    isCompleted: true,
+    index: 1,
+  },
+];
 
-  return element;
-}
+const getTaskIndex = (index) => {
+  for (let i = 0; i <listItems.length; i += 1) {
+    if (listItems[i].index === index) {
+      return listItems[i];
+    }
+  }
+  return null;
+};
 
-document.body.appendChild(component());
+const populateTasks = () => {
+  for (let i = 0; i < listItems.length; i += 1) { 
+    const currentTask = getTaskIndex(i);
+    const newLi = document.createElement('li');
+    const checkbox = document.createElement('INPUT');
+    const taskName = document.createElement('INPUT');
+    const taskButton = document.createElement('button');
+    checkbox.setAttribute('type', 'checkbox');
+    taskName.setAttribute('type', 'text');
+    tasksDiv.append(newLi);
+    newLi.append(checkbox, taskName, taskButton);
+    taskName.value = currentTask.description
+    checkbox.checked = currentTask.isCompleted;
+  }
+};
+populateTasks();
