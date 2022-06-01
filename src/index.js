3968fcb7c81bf143e8a7ed59c64a0c38665b7443
Gabriel <gopxfs@hotmail.com>
2022-06-01 01:43:55 -0300

    Add addtask function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // import _ from 'lodash';
 import './style.css';
 
+// Dinamic creation of the list
 const tasksDiv = document.getElementById('to-do-list');
 const listTitleDiv = document.createElement('div');
 const listTitle = document.createElement('INPUT');
@@ -9,7 +10,6 @@ const addTaskDiv = document.createElement('div');
 const addTask = document.createElement('INPUT');
 const enterTask = document.createElement('button');
 const clearCompleted = document.createElement('button');
-
 listTitle.setAttribute('type', 'text');
 addTask.setAttribute('type', 'text');
 listTitle.value = 'Today\'s To Do';
@@ -20,26 +20,12 @@ addTaskDiv.append(addTask, enterTask);
 clearCompleted.innerHTML = 'Clear all completed';
 clearCompleted.setAttribute('id', 'clearCompleted');
 
+// description isCompleted index
 const listItems = [
   {
-    description: 'Task 1',
+    description: '6546541asaass',
     isCompleted: false,
     index: 0,
-  },
-  {
-    description: 'Task 2',
-    isCompleted: false,
-    index: 3,
-  },
-  {
-    description: 'Task 3',
-    isCompleted: true,
-    index: 2,
-  },
-  {
-    description: 'Task 4',
-    isCompleted: true,
-    index: 1,
   },
 ];
 
@@ -52,20 +38,36 @@ const getTaskIndex = (index) => {
   return null;
 };
 
+const createLi = (task) => {
+  const newLi = document.createElement('li');
+  const checkbox = document.createElement('INPUT');
+  const taskName = document.createElement('INPUT');
+  const taskButton = document.createElement('button');
+  checkbox.setAttribute('type', 'checkbox');
+  taskName.setAttribute('type', 'text');
+  tasksDiv.append(newLi);
+  newLi.append(checkbox, taskName, taskButton);
+  taskName.value = task.description;
+  checkbox.checked = task.isCompleted;
+};
+
+const createTask = (description) => {
+  const task = {
+    description: description,
+    isCompleted: false,
+    index: listItems.length,
+  };
+  return task;
+};
+
 const populateTasks = () => {
   for (let i = 0; i < listItems.length; i += 1) {
-    const currentTask = getTaskIndex(i);
-    const newLi = document.createElement('li');
-    const checkbox = document.createElement('INPUT');
-    const taskName = document.createElement('INPUT');
-    const taskButton = document.createElement('button');
-    checkbox.setAttribute('type', 'checkbox');
-    taskName.setAttribute('type', 'text');
-    tasksDiv.append(newLi);
-    newLi.append(checkbox, taskName, taskButton);
-    taskName.value = currentTask.description;
-    checkbox.checked = currentTask.isCompleted;
+    createLi(listItems[i]);
   }
 };
 populateTasks();
 tasksDiv.append(clearCompleted);
+enterTask.addEventListener('click', () => {
+  const newTask = createTask(addTask.value);
+  createLi(newTask);
+});
